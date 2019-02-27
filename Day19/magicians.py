b410a3986b369d16067d5a35f0e54e814ce73602
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:40:57 +0100

    Create magicians.py

---
+++ Day19/magicians.py
@@ -0,0 +1,5 @@
+magicians = ['saqib','ali','imran']
+for magician in magicians:
+  print(magician + ", that was a great trick!")
+  print("I can't wait to see your next trick,"+magician +".\n ")
+  print("Thank you, everyone. That was a great magic show")
