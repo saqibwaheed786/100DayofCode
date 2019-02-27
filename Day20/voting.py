05a3331727ac4efcce4c4bb7df996d1295f9a016
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 12:02:41 +0100

    Create voting.py

---
+++ Day20/voting.py
@@ -0,0 +1,7 @@
+age = 17
+if age >= 18:
+  print("you are old enough to vote")
+  print("Have you registered to vote yet?")
+else:
+  print("Sorry, you are to young to vote")
+  print("Please reqister to vote as soon as turn 18")
