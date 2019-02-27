937bcd2f1d5fc275bf2760e204dfd4b716e96b17
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 12:04:41 +0100

    Create toppings2.py

---
+++ Day20/toppings2.py
@@ -0,0 +1,8 @@
+requested_toppings =['mushrooms','extra cheese']
+if 'mushrooms' in requested_toppings:
+  print("Adding mushrooms.")
+  if 'papperoni' in requested_toppings:
+    print("Adding papperoni")
+    if 'extra cheese' in requested_toppings:
+      print("Adding extra cheese")
+      print("\nFinished making your pizza!")
