cd6aec25785de8a8185953d139436f1441db3b60
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 20:59:03 +0100

    Create favorite_languages.py

---
+++ Day21/favorite_languages.py
@@ -0,0 +1,8 @@
+                       # A Dictionary of Similar Objects
+favorite_languages ={
+'saqib':'python',
+'sarah':'c',
+'edward':'ruby',
+'phil':'python'}
+print("Saqib's favorite languages is " +
+  favorite_languages['saqib'].title() + ".")
