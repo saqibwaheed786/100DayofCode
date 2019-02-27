5b64cd7a87fc907075e8c7142b6cac3df7374e23
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:19:58 +0100

    Create name.py

---
+++ Day16/name.py
@@ -0,0 +1,20 @@
+name = "ada lovelace"
+#print(name.title())
+#print(name.upper())
+#print(name.lower())
+
+
+#first_name = "ada"
+#last_name ="lovelace"
+#full_name = first_name +  " " +last_name
+#message = ("Hello, " + full_name.title() + "!" )
+#print(message)
+
+#print("Language:\n\tPython\n\tC\n\tJavaScript")
+# \n is newline
+#\t tab
+
+favorite_language= ' Python'  
+
+favorite_language.rstrip()
+print(favorite_language)
