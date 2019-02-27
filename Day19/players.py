01bbb8109a3cba4f389f17f01d7146c7e900f9a9
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:44:11 +0100

    Create players.py

---
+++ Day19/players.py
@@ -0,0 +1,8 @@
+#Slicing a List
+
+
+#Looping Through a Slice
+players = ['charles', 'martina', 'michael','florence','eli']
+print("Here are the first three players on my team: ")
+for player in players[:3]:
+  print(player.title())
