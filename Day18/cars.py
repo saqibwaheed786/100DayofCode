33ed9f57d16da7dc95fa6fd146eba59b39dd7869
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:36:39 +0100

    Create cars.py

---
+++ Day18/cars.py
@@ -0,0 +1,34 @@
+#Organizing a List
+#Sorting a List Permanently with the sort() Method
+#..................................................................
+cars = ['bmw', 'audi', 'toyata', 'subaru']
+#cars.sort()
+#print(cars)
+
+#reverse alphabetical order
+#...................................................................
+#cars.sort(reverse=True)
+#print(cars)
+
+#print('Here is the original list:')
+#print(cars)
+
+#print('\n Hear is the sorted list:')
+#print(sorted(cars))
+#print("\n Here is the original list again: ")
+
+#Printing a List in Reverse Order
+#........................................................................
+#print(cars)
+#cars.reverse()
+#print(cars)
+#can revert to the original order anytime by applying reverse() to the same
+#list a second time.
+#cars.reverse()
+#print(cars)
+
+#Finding the Length of a List
+#........................................................................
+cars = ['bmw', 'audi', 'toyata', 'subaru']
+len(cars)
+#........................................................................
