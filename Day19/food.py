fff2761ab74a098880a1205b64b2fac423305d9b
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:44:51 +0100

    Create food.py

---
+++ Day19/food.py
@@ -0,0 +1,10 @@
+my_food = ['pizza', 'falafel', 'carrot cake']
+friend_food = my_food[:]
+
+my_food.append("cannoli")
+friend_food.append("ice crem")
+print("My Favorite food are:")
+print(my_food)
+
+print("\nMy friend's favorite foods are ")
+print(friend_food)
