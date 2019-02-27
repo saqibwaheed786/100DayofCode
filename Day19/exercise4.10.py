bac50fe019ee431354adb2c4f08f24a8a7ee07f1
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:52:52 +0100

    Create exercise4.10.py

---
+++ Day19/exercise4.10.py
@@ -0,0 +1,7 @@
+grocery_list = ['Tomato sauce','Mustard','Barbecue sauce','Salsa','honey','ketchup']
+print("The first three items in the list are:.")
+print(grocery_list[:3] )
+print("\nThree items from the middle of the list are:.")
+print(grocery_list[1:4])
+print("\nThe last three items in the list are:.")
+print(grocery_list[3:])
