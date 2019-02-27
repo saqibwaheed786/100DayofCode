64bac1dbdd4dc005024e525819daae7f9b41de56
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-02-27 11:41:56 +0100

    Create numbers.py

---
+++ Day19/numbers.py
@@ -0,0 +1,7 @@
+#you can use the range() function to print a series of numbers
+# like this
+#for value in range(1,5):
+  #print(value)
+#Using range() to Make a List of Numbers
+numbers = list(range(1,6))
+print(numbers)
