b31f26cfb72091e05e6dc85dd4388c5a0eb4a5a2
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-03-07 12:52:28 +0100

    Create exercise7.1.py

---
+++ Day25/exercise7.1.py
@@ -0,0 +1,6 @@
+# 7-1. Rental Car: Write a program that asks the user what kind of rental car they
+# would like. Print a message about that car, such as “Let me see if I can find you
+# a BMW.”
+# -------------------------------------------------------------------------------------
+rentel_car = input("Let me see if i can find you a BMW")
+print(rentel_car)
