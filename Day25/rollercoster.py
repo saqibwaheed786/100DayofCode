a37b3bca049973a19402d55285584e1976859a30
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-03-07 11:57:50 +0100

    Create rollercoster.py

---
+++ Day25/rollercoster.py
@@ -0,0 +1,9 @@
+# Usint int() to Accept Numerical Input
+# -------------------------------------------------------------------------------
+height = input("How tall are you, in inches?")
+height = int(height)
+
+if height >= 36:
+  print("\nYou're tall enough to ride! ")
+else:
+  print("\nYou'll ve able to ride when you're a little older.")
