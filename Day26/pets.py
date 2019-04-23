72e61ef0bdf2c18c1012cca16993524c121d0eb8
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-04-23 13:01:54 +0200

    Create pets.py

---
+++ Day26/pets.py
@@ -0,0 +1,6 @@
+#Removing All Instances of Specific Values from a List
+pets = ['dog', 'cat', 'dog', 'goldfish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+  pets.remove('cat')
+  print(pets)
