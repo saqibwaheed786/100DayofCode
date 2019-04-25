2dc49a8d11c126e1fd23376ec0dc79f81422b261
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-04-25 10:37:18 +0200

    Delete formatted_name.py

--- Day28/formatted_name.py
+++
@@ -1,7 +0,0 @@
-def get_formatted_name(first_name, last_name):
-  """Return a full name, neastly formatted."""
-  full_name = first_name + '' + last_name
-  return full_name.title()
-
-musician = get_formatted_name('saqib ', 'malik ')
-print(musician)   
