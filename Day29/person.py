1cff98f5e6ad5d599fa3989b4eb960bf6fca22f4
Saqib Waheed <38664302+saqibwaheed786@users.noreply.github.com>
2019-04-25 11:05:18 +0200

    Create person.py

---
+++ Day29/person.py
@@ -0,0 +1,8 @@
+def build_person(first_name,last_name):
+  """Return a dictionary of information about a person."""
+  person = {'first': first_name, 'last': last_name}
+  return person
+
+musician = build_person('zeeshan', 'ahmad')
+print(musician)  
+  
