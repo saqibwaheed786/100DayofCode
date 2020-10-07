621feaa5823478e6148f5bbf5c466323a4acf9e4
saqibwaheed <personalmail786@gmail.com>
2020-10-07 13:21:57 +0200

    updating exercise 8.2

--- DAY28/exercise8.1.py
+++ DAY28/exercise8.1.py
@@ -1,6 +1,6 @@
 def display_message():
   """Display a message about what i am learning."""
-  msg = "i am learnig to store code in function."
+  msg = "i am learning to store code in function."
   print(msg)
 
 display_message()
